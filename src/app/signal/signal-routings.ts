8126f6f47991d00feca3feca9a1307df7a37ff3d
Steven <steven.mejia@gmail.com>
2024-08-13 20:30:29 -0500

    WIP

--- src/app/signal/signal-routings.ts
+++ src/app/signal/signal-routings.ts
@@ -24,6 +24,21 @@ const ROUTES: Route[] = [
                 path: 'signal-ejemplo3',
                 loadComponent: () =>  
                 import('./signal-ejemplo3/signal-ejemplo3.component')
+            },
+            {
+                path: 'signal-ejemplo4',
+                loadComponent: () =>  
+                import('./signal-ejemplo4/signal-ejemplo4.component')
+            },
+            {
+                path: 'signal-ejemplo5',
+                loadComponent: () =>  
+                import('./signal-ejemplo5/signal-ejemplo5.component')
+            },
+            {
+                path: 'signal-ejemplo6',
+                loadComponent: () =>  
+                import('./signal-ejemplo6/signal-ejemplo6.component')
             }
         ]
     }
